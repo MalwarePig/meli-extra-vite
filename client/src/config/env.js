4c6579daaa8554c2e0a074ebfd6c498f1210577c
Malwarepig <sergio_lcc92@hotmail.com>
2025-05-05 22:57:10 -0600

    first commit

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -1,1 +1,1 @@
-export const backend_url = 'http://localhost:4000' ||'https://meli-extra-vite.onrender.com'
+export const backend_url = /* 'http://localhost:4000' */  'https://meli-extra-vite.onrender.com'
